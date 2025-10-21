968656070834f0f4c8122547c956540d6403a6ba
amitkumar1075 <amitkumar1075a@gmail.com>
2025-10-21 15:47:56 +0530

    initial commits

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load trained model
+model = pickle.load(open('model.pkl', 'rb'))
+
+# Home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Form se values le lo
+        features = [float(x) for x in request.form.values()]
+        final_features = [np.array(features)]
+        
+        # Prediction karo
+        prediction = model.predict(final_features)
+        output = round(prediction[0], 2)
+        
+        return render_template('index.html', prediction_text=f'Predicted Price: ${output} (x100000)')
+    except Exception as e:
+        return render_template('index.html', prediction_text=f'Error: {e}')
+
+if __name__ == "__main__":
+    app.run(debug=True)
